b13df52d02d22c8cd44abf7deb033dbf406d888a
wiwka2 <dimkaakadiem@gmail.com>
2025-01-17 23:19:34 +1000

    Small fix in Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -10,7 +10,7 @@ use const Php\Project\GameEngine\MAX_NUMBER;
 
 function runGamePrime(): void
 {
-    $gameRules = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"";
+    $gameRules = "Answer \"yes\" if given number is prime. Otherwise answer \"no\".";
     $gameData = [];
 
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
